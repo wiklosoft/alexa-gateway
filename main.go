06b32eb0a1759a301128b87b2cfd40af5efa9f55
pawel.wiklowski <pawel.wiklowski@gmail.com>
2017-05-01 22:06:34 +0200

    Start implementing ws hub interface

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 	"container/list"
 
-	"strings"
+	"strconv"
 
 	"gopkg.in/kataras/iris.v6"
 	"gopkg.in/kataras/iris.v6/adaptors/httprouter"
@@ -37,9 +37,37 @@ type AuthUserData struct {
 	Username string `json:"username"`
 }
 
+type IotVariable struct {
+	Interface    string
+	ResourceType string
+	Href         string
+	Name         string
+}
+
+type IotDevice struct {
+	ID        string
+	Name      string
+	Variables []IotVariable
+}
+
 type ClientConnection struct {
 	Username   string
 	Connection websocket.Connection
+	DeviceList []IotDevice
+
+	Callbacks map[int]RequestCallback
+	Mid       int
+}
+
+type RequestCallback func(string)
+
+type IotPayload struct {
+	Request string `json:"request"`
+}
+type IotMessage struct {
+	Mid     int        `json:"mid"`
+	Payload IotPayload `json:"payload"`
+	Event   string     `json:"event"`
 }
 
 func getUserInfo(token string) (user AuthUserData, e error) {
@@ -64,14 +92,16 @@ func getUserInfo(token string) (user AuthUserData, e error) {
 	buf.ReadFrom(resp.Body)
 	r := buf.String()
 
-	fmt.Println(r)
-
 	if err := json.Unmarshal([]byte(r), &userData); err != nil {
 		log.Fatal(err)
 	}
-
-	fmt.Println(userData)
 	return userData, nil
+}
+
+func sendRequest(conn *ClientConnection, request string, callback RequestCallback) {
+	conn.Callbacks[conn.Mid] = callback
+	conn.Connection.EmitMessage([]byte(`{ "mid":` + strconv.Itoa(conn.Mid) + `, "payload":{"request":"` + request + `"}}`))
+	conn.Mid++
 }
 
 func main() {
@@ -87,25 +117,29 @@ func main() {
 
 	ws.OnConnection(func(c websocket.Connection) {
 		log.Println("New connection", c.ID())
-		newConnection := &ClientConnection{"", c}
+		newConnection := &ClientConnection{Username: "",
+			Connection: c,
+			Mid:        1,
+			Callbacks:  make(map[int]RequestCallback)}
 		clientConnections.PushBack(newConnection)
+
+		sendRequest(newConnection, "RequestGetDevices", func(response string) {
+			log.Println("Callback response" + response)
+		})
+
 		c.OnMessage(func(messageBytes []byte) {
-			message := string(messageBytes)
+			message := &IotMessage{}
 
-			messageType := strings.Split(message, ":")[0]
-			messagePayload := strings.Split(message, ":")[1]
-
-			if messageType == "auth" {
-				log.Println("Auth request on connection ", c.ID(), " - ", messagePayload)
-				userInfo, err := getUserInfo(messagePayload)
-				if err != nil {
-					fmt.Println(err)
-					return
-				}
-				newConnection.Username = userInfo.Username
-				log.Println("Saved username: " + newConnection.Username)
+			if err := json.Unmarshal(messageBytes, &message); err != nil {
+				fmt.Println(err)
 			}
+			log.Println(message)
 
+			callback := newConnection.Callbacks[message.Mid]
+			if callback != nil {
+				callback("message")
+				delete(newConnection.Callbacks, message.Mid)
+			}
 		})
 
 		c.OnDisconnect(func() {
